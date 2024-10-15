1efecddbefa2f2be0223eb02745b8d32f025bbe4
eoeo <eoeo@localhost.localdomain>
2024-10-15 20:11:14 +0300

    front end

---
+++ src/services/notes.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/notes/'
+
+const getAll = () =>{
+  const result = axios.get(baseUrl)
+  const nonExistant = {
+    id:1100,
+    content:'this note not exist',
+    important:true
+  }
+  return result.then(response=> response.data.concat(nonExistant))
+}
+
+
+const create = newObject =>{
+  const request=  axios.post(baseUrl,newObject)
+  return request.then(response => response.data)
+}
+
+const update = (id,newObject) => {
+  // console.log(id)
+  const request =  axios.put(`${baseUrl}/${id}`,newObject)
+  // console.log(id)
+  return request.then(response=> response.data)
+}
+
+export default {
+  getAll,create,update
+}
